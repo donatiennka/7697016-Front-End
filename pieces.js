8ff402bf3a9c015c17a94cd0b4aab554b2c532e3
donatech <donatien.nka@gmail.com>
2023-10-24 15:21:10 +0100

    Réecriture du code du fichier pieces.js avec la bouble for.

--- pieces.js
+++ pieces.js
@@ -2,32 +2,42 @@
 const reponse = await fetch("pieces-autos.json");
 const pieces = await reponse.json();
 
-//Création des balises html dans notre code grâce à document.createElement
-const article = pieces[0];
-const imageElement = document.createElement("img");
-imageElement.src = article.image;
-const nomElement = document.createElement("h2");
-nomElement.innerText = article.nom;
-const prixElement = document.createElement("p");
-// Ici on utilise un template strings avec les backticks "`"
-//prixElement.innerText = `Prix: ${article.prix} FCFA`;
-prixElement.innerText = `Prix: ${article.prix} FCFA (${article.prix < 35 ? "moins cher !" : "cher !"})`;
+for (let i = 0; i < pieces.length; i++) {
 
-const categorieElement = document.createElement("p");
-// Founir une valeur par défaut grâce à l'opérateur nullish (??)
-categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
+    // Récupération de l'élément du DOM qui accueillera les fiches
+    const sectionFiches = document.querySelector(".fiches");
+    // Création d'une balise dédiée à une pièce automobile
+    const pieceElement = document.createElement("article");
+    // On crée l'élément img.
+    const imageElement = document.createElement("img");
+    // On accède à l'indice i de la liste pieces pour configurer la source de l'image.
+    imageElement.src = pieces[i].image;
+    // On crée l'élément du nom
+    const nomElement = document.createElement("h2");
+    // On accède à l'indice i de la liste pieces pour configurer le nom
+    nomElement.innerText = pieces[i].nom;
+    // On crée l'élément du prix
+    const prixElement = document.createElement("p");
+    // On accède à l'indice i de la liste pieces pour configurer le prix
+    prixElement.innerText = `Prix: ${pieces[i].prix} FCFA (${pieces[i].prix < 35 ? "moins cher !" : "cher !"})`;
+    // On crée l'élément de la catégorie
+    const categorieElement = document.createElement("p");
+    // Founir une valeur par défaut grâce à l'opérateur nullish (??)
+    categorieElement.innerText = pieces[i].categorie ?? "(aucune catégorie)";
+    // On crée l'élément de la description
+    const descriptionElement = document.createElement("p");
+    descriptionElement.innerText = pieces[i].description ?? "Pas de description pour le moment.";
+    // On crée l'élément de la disponibilité
+    const stockElement = document.createElement("p");
+    stockElement.innerText = `${pieces[i].disponibilite === true ? "En stock" : "Rupture de stock"}`;
 
-const descriptionElement = document.createElement("p");
-descriptionElement.innerText = article.description ?? "Pas de description pour le moment.";
-const stockElement = document.createElement("p")
-stockElement.innerText = `${article.disponibilite === true ? "En stock" : "Rupture de stock"}`
-
-// Récupération de la balise parent à laquelle seront rattachées les balise créées
-const sectionFiches = document.querySelector(".fiches");
-// Ensuite on fait le rattachemet grâce à "appendChild"
-sectionFiches.appendChild(imageElement);
-sectionFiches.appendChild(nomElement);
-sectionFiches.appendChild(prixElement);
-sectionFiches.appendChild(categorieElement);
-sectionFiches.appendChild(descriptionElement);
-sectionFiches.appendChild(stockElement);
+    // On rattache la balise article à la section fiches
+    sectionFiches.appendChild(pieceElement);
+    // On rattache les éléments à pièceElement (la balise article)
+    pieceElement.appendChild(imageElement);
+    pieceElement.appendChild(nomElement);
+    pieceElement.appendChild(prixElement);
+    pieceElement.appendChild(categorieElement);
+    pieceElement.appendChild(descriptionElement);
+    pieceElement.appendChild(stockElement);
+}
