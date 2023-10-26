2c37852fe167fb20bd9dfdd50605df00c7c3227c
donatech <donatien.nka@gmail.com>
2023-10-26 12:44:26 +0100

    Mise à jour de la page web grâce à innerHTML...

--- pieces.js
+++ pieces.js
@@ -1,144 +1,149 @@
 // Récupération des pièces depuis le fichier JSON
-const reponse = await fetch("pieces-autos.json");
+const reponse = await fetch('pieces-autos.json');
 const pieces = await reponse.json();
 
-for (let i = 0; i < pieces.length; i++) {
+// On crée une fonction qui se charge de générer toute la page web
+function genererPieces(pieces){
+    for (let i = 0; i < pieces.length; i++) {
 
-    // Récupération de l'élément du DOM qui accueillera les fiches
-    const sectionFiches = document.querySelector(".fiches");
-    // Création d'une balise dédiée à une pièce automobile
-    const pieceElement = document.createElement("article");
-    // On crée l'élément img.
-    const imageElement = document.createElement("img");
-    // On accède à l'indice i de la liste pieces pour configurer la source de l'image.
-    imageElement.src = pieces[i].image;
-    // On crée l'élément du nom
-    const nomElement = document.createElement("h2");
-    // On accède à l'indice i de la liste pieces pour configurer le nom
-    nomElement.innerText = pieces[i].nom;
-    // On crée l'élément du prix
-    const prixElement = document.createElement("p");
-    // On accède à l'indice i de la liste pieces pour configurer le prix
-    prixElement.innerText = `Prix: ${pieces[i].prix} FCFA (${pieces[i].prix < 35 ? "moins cher !" : "cher !"})`;
-    // On crée l'élément de la catégorie
-    const categorieElement = document.createElement("p");
-    // Founir une valeur par défaut grâce à l'opérateur nullish (??)
-    categorieElement.innerText = pieces[i].categorie ?? "(aucune catégorie)";
-    // On crée l'élément de la description
-    const descriptionElement = document.createElement("p");
-    descriptionElement.innerText = pieces[i].description ?? "Pas de description pour le moment.";
-    // On crée l'élément de la disponibilité
-    const stockElement = document.createElement("p");
-    stockElement.innerText = `${pieces[i].disponibilite === true ? "En stock" : "Rupture de stock"}`;
-
-    // On rattache la balise article à la section fiches
-    sectionFiches.appendChild(pieceElement);
-    // On rattache les éléments à pièceElement (la balise article)
-    pieceElement.appendChild(imageElement);
-    pieceElement.appendChild(nomElement);
-    pieceElement.appendChild(prixElement);
-    pieceElement.appendChild(categorieElement);
-    pieceElement.appendChild(descriptionElement);
-    pieceElement.appendChild(stockElement);
+        const article = pieces[i];
+        // Récupération de l'élément du DOM qui accueillera les fiches
+        const sectionFiches = document.querySelector(".fiches");
+        // Création d’une balise dédiée à une pièce automobile
+        const pieceElement = document.createElement("article");
+        // Création des balises 
+        const imageElement = document.createElement("img");
+        imageElement.src = article.image;
+        const nomElement = document.createElement("h2");
+        nomElement.innerText = article.nom;
+        const prixElement = document.createElement("p");
+        prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
+        const categorieElement = document.createElement("p");
+        categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
+        const descriptionElement = document.createElement("p");
+        descriptionElement.innerText = article.description ?? "Pas de description pour le moment.";
+        const stockElement = document.createElement("p");
+        stockElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
+        
+        // On rattache la balise article a la section Fiches
+        sectionFiches.appendChild(pieceElement);
+        // On rattache l’image à pieceElement (la balise article)
+        pieceElement.appendChild(imageElement);
+        pieceElement.appendChild(nomElement);
+        pieceElement.appendChild(prixElement);
+        pieceElement.appendChild(categorieElement);
+        //Ajout des éléments au DOM pour l'exercice
+        pieceElement.appendChild(descriptionElement);
+        pieceElement.appendChild(stockElement);
+    
+     }
 }
 
-// Ajoutons un listner au bouton "Trier par prix croissants"
-const boutonTrier = document.querySelector(".btn-trier");
+// Premier affichage de la page
+genererPieces(pieces);
 
+//gestion des bouttons 
+const boutonTrier = document.querySelector(".btn-trier");
+// Ajout du listener pour trier les pièces par ordre de prix croissant
 boutonTrier.addEventListener("click", function () {
     const piecesOrdonnees = Array.from(pieces);
     piecesOrdonnees.sort(function (a, b) {
         return a.prix - b.prix;
-    });
-    console.log(piecesOrdonnees);
+     });
+     // Effacement de l'écran et regénération de la page
+     document.querySelector(".fiches").innerHTML = "";
+    genererPieces(piecesOrdonnees);
 });
 
-// Ajoutons un listner au bouton "Trier par prix décroissants"
-const boutonDecroissant = document.querySelector(".btn-decroissant");
-
-boutonDecroissant.addEventListener("click", function () {
-    const piecesOrdonnees = Array.from(pieces);
-    piecesOrdonnees.sort(function (a, b) {
-        return b.prix - a.prix;
-    });
-    console.log(piecesOrdonnees);
-});
-
-//Ajoutons un listner sur le bouton Filtrer... pour n'afficher que les pièces dont le prix
-// est inférieur ou égal à 35 FCFA
 const boutonFiltrer = document.querySelector(".btn-filtrer");
 
 boutonFiltrer.addEventListener("click", function () {
     const piecesFiltrees = pieces.filter(function (piece) {
         return piece.prix <= 35;
     });
-
-    console.log(piecesFiltrees)
+    document.querySelector(".fiches").innerHTML = "";
+    genererPieces(piecesFiltrees);
 });
 
-// Filtrons pour n'afficher que les pièces qui ont un descriptif
+//Correction Exercice
+const boutonDecroissant = document.querySelector(".btn-decroissant");
+
+boutonDecroissant.addEventListener("click", function () {
+    const piecesOrdonnees = Array.from(pieces);
+    piecesOrdonnees.sort(function (a, b) {
+        return b.prix - a.prix;
+     });
+     document.querySelector(".fiches").innerHTML = "";
+    genererPieces(piecesOrdonnees);
+});
+
 const boutonNoDescription = document.querySelector(".btn-nodesc");
 
 boutonNoDescription.addEventListener("click", function () {
     const piecesFiltrees = pieces.filter(function (piece) {
-        return piece.description;
+        return piece.description
     });
-
-    console.log(piecesFiltrees)
+    document.querySelector(".fiches").innerHTML = "";
+    genererPieces(piecesFiltrees);
 });
 
-// Nous décidons d'afficher uniquement les noms des pièces
 const noms = pieces.map(piece => piece.nom);
-console.log(noms);
-
-// Maintenant nous suprimons de la liste des noms les pièces qui ne sont pas abordables
-for(let i = pieces.length - 1; i >= 0; i--) {
-    if (pieces[i].prix > 35) {
-        noms.splice(i, 1)
+for(let i = pieces.length -1 ; i >= 0; i--){
+    if(pieces[i].prix > 35){
+        noms.splice(i,1)
     }
 }
-console.log(noms);
+console.log(noms)
+//Création de l'en-tête
 
-// Nous pouvons afficher la liste des pièces abordables dans une liste à puce
-// Création de la liste
-const abordablesElements = document.createElement("ul");
-// Ajout de chaque nom à la liste
-for (let i = 0; i < noms.length; i++) {
-    const nomElement = document.createElement("li");
+const pElement = document.createElement('p')
+pElement.innerText = "Pièces abordables";
+//Création de la liste
+const abordablesElements = document.createElement('ul');
+//Ajout de chaque nom à la liste
+for(let i=0; i < noms.length ; i++){
+    const nomElement = document.createElement('li');
     nomElement.innerText = noms[i];
     abordablesElements.appendChild(nomElement)
 }
 // Ajout de l'en-tête puis de la liste au bloc résultats filtres
-document.querySelector(".abordables")
+document.querySelector('.abordables')
+    .appendChild(pElement)
     .appendChild(abordablesElements)
 
+//Code Exercice 
+const nomsDisponibles = pieces.map(piece => piece.nom)
+const prixDisponibles = pieces.map(piece => piece.prix)
 
-
-
-// Nous voulons une liste des pièces disponibles
-let piecesDisponibles = []
-
-for(let i = 0; i < pieces.length; i++) {
-    if (pieces[i].disponibilite) {
-       piecesDisponibles.push(pieces[i])
+for(let i = pieces.length -1 ; i >= 0; i--){
+    if(pieces[i].disponibilite === false){
+        nomsDisponibles.splice(i,1)
+        prixDisponibles.splice(i,1)
     }
 }
-console.log(piecesDisponibles);
 
-// Nous décidons d'afficher uniquement les noms des pièces disponibles avec leurs prix
-const nomsEtPrixDispo = piecesDisponibles.map(piece => `${piece.nom} - ${piece.prix} FCFA`);
-console.log(nomsEtPrixDispo);
+const disponiblesElement = document.createElement('ul');
 
-
-// Maintenant nous voulons afficher la liste des pièces disponibles dans une liste à puce
-// Création de la liste
-const disponiblesElements = document.createElement("ul");
-// Ajout de chaque nom à la liste
-for (let i = 0; i < nomsEtPrixDispo.length; i++) {
-    const nomElement = document.createElement("li");
-    nomElement.innerText = nomsEtPrixDispo[i];
-    disponiblesElements.appendChild(nomElement)
+for(let i=0 ; i < nomsDisponibles.length ; i++){
+    const nomElement = document.createElement('li');
+    nomElement.innerText = `${nomsDisponibles[i]} - ${prixDisponibles[i]} €`
+    disponiblesElement.appendChild(nomElement)
 }
-// Ajout de l'en-tête puis de la liste au bloc résultats fiches produits
-document.querySelector(".disponibles")
-    .appendChild(disponiblesElements)
+
+const pElementDisponible = document.createElement('p')
+pElementDisponible.innerText = "Pièces disponibles:";
+document.querySelector('.disponibles').appendChild(pElementDisponible).appendChild(disponiblesElement)
+
+//Exercice 
+// Ajout d'un listener à notre balise input
+const inputPrixMax = document.querySelector("#prix-max");
+
+// On ajoute un listener de type input qui permet de recupérer une valeur dans 
+// le champ input sans avoir besoin de clicker sur validé
+inputPrixMax.addEventListener("input", function () {
+    const piecesFiltrees = pieces.filter(function(piece) {
+        return piece.prix <= inputPrixMax.value;
+    });
+    document.querySelector(".fiches").innerHTML = "";
+    genererPieces(piecesFiltrees)
+})
